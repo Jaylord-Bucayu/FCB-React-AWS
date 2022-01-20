a722bcee3dad3b7d3991ed77042995194049d045
jaylordbubucayu <57398270+jaylordbubucayu@users.noreply.github.com>
2022-01-20 15:32:06 +0800

    Hello World React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDom from "react-dom"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function HelloWorld(){
+
+    return <h2>Hello World</h2>
+}
+
+ReactDom.render(<HelloWorld/>,document.getElementById("root"));
