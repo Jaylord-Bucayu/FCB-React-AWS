7bb11162cbe8e4508ecf7f12196012367cd70bbd
jaylordbubucayu <57398270+jaylordbubucayu@users.noreply.github.com>
2022-01-21 15:23:35 +0800

    Initial commit

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -5,22 +5,44 @@ import Task from "./Task";
 
 function TaskList(){
 
-
-    const taskItems = ["Follow Edukasyon Ph on Facebook","Follow Aws Siklab on Facebook","Follow Zuitt on Facebook"]
+   //data to be used
+    const [taskItemsList, setTaskItemsList] = useState([
+        "Follow Edukasyon.ph on Facebook.", // 0
+        "Follow AWS Siklab Pilipinas on Facebook.", // 1
+        "Follow Zuitt Coding Bootcamp on Facebook." // 2
+      ]);
    
+    //to add to task value   
    const [taskValue,setTaskValue] = useState();
 
-   const inputChangeHandler = (e) =>{
    
-    setTaskValue();
-    }
+   //when change set new task
+    const inputChangeHandler = (e) => {
 
+       
+        setTaskValue(e.target.value)
+      };
+    
+    //add the task value to the list of task
+    const addTaskHandler = (e) => {
+
+        if(e.keyCode == 13){
+            setTaskItemsList([taskValue, ...taskItemsList]);
+
+            e.target.value = '';
+        }
+       
+       
+
+      };
+
+      
 return(
  
 <>
-<input className="task-input" placeholder="Enter new Task" onChange={inputChangeHandler}/>
+<input className="task-input" placeholder="Enter new Task" value={taskValue}  onChange={inputChangeHandler} onKeyUp={addTaskHandler}/>
     <ul>
-    {taskItems.map((task,index) => {
+    {taskItemsList.map((task,index) => {
         return (<Task task={task} key={index} />);
         
     })}
