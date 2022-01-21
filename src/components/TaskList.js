745ffc04a46d7dc352bbf65abbe7048c1d01cc21
jaylordbubucayu <57398270+jaylordbubucayu@users.noreply.github.com>
2022-01-21 15:00:00 +0800

    Create the inputhandler and state

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,25 +1,39 @@
-import React from "react"
+import { useState } from "react";
 
 import Task from "./Task";
+
 
 function TaskList(){
 
 
     const taskItems = ["Follow Edukasyon Ph on Facebook","Follow Aws Siklab on Facebook","Follow Zuitt on Facebook"]
    
+   const [taskValue,setTaskValue] = useState();
+
+   const inputChangeHandler = (e) =>{
    
-return( <ul>
+    setTaskValue();
+    }
+
+return(
+ 
+<>
+<input className="task-input" placeholder="Enter new Task" onChange={inputChangeHandler}/>
+    <ul>
     {taskItems.map((task,index) => {
         return (<Task task={task} key={index} />);
         
     })}
 
-</ul>);
+</ul>
+</>
+);
       
     
    
+  
  
 
-   }
+     }
 
 export default TaskList;
